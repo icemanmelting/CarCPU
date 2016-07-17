3ec019f46ada9ebcb3dead3e32a8b5e2206922b8
iceman <icemanmelting@gmail.com>
2016-07-17 21:01:15 +0100

    initial commit

---
+++ src/main/java/pt/iceman/carcpu/CarCPU.java
@@ -0,0 +1,7 @@
+package pt.iceman.carcpu;
+
+/**
+ * Created by iceman on 17/07/2016.
+ */
+public class CarCPU {
+}
---
+++ src/main/java/pt/iceman/carcpu/modules/Module.java
@@ -0,0 +1,8 @@
+package pt.iceman.carcpu.modules;
+
+/**
+ * Created by iceman on 17/07/2016.
+ */
+public interface Module {
+    public void interpretCommand();
+}
