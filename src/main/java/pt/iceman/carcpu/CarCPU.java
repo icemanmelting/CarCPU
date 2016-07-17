b7223baf9005310dd1bcb57d098bc26e2bb46432
iceman <icemanmelting@gmail.com>
2016-07-17 21:17:48 +0100

    added initial modules

--- src/main/java/pt/iceman/carcpu/CarCPU.java
+++ src/main/java/pt/iceman/carcpu/CarCPU.java
@@ -1,7 +1,34 @@
 package pt.iceman.carcpu;
 
+import org.reflections.Reflections;
+import pt.iceman.carcpu.modules.Module;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by iceman on 17/07/2016.
  */
 public class CarCPU {
+    private static Map<Class<? extends Module >, Module> modules;
+
+    public Map<Class<? extends Module >, Module> getModules() {
+        if(modules == null) {
+            modules = new HashMap<>();
+            Reflections reflections = new Reflections("pt.iceman.carcpu.modules");
+
+            Set<Class<? extends Module>> allClasses =
+                    reflections.getSubTypesOf(Module.class);
+
+            allClasses.forEach(c -> {
+                try {
+                    modules.put(c, c.newInstance());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        return modules;
+    }
 }
---
+++ src/main/java/pt/iceman/carcpu/modules/Lights.java
@@ -0,0 +1,11 @@
+package pt.iceman.carcpu.modules;
+
+/**
+ * Created by iceman on 17/07/2016.
+ */
+public class Lights implements Module {
+    @Override
+    public void interpretCommand() {
+
+    }
+}
