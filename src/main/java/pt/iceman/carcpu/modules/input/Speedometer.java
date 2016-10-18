21ecdd1b1077c03c119fe2b651767d69cd9d2e7a
iceman <icemanmelting@gmail.com>
2016-10-18 23:38:02 +0100

    fixed bug with speed value being erratic while car not moving

--- src/main/java/pt/iceman/carcpu/modules/input/Speedometer.java
+++ src/main/java/pt/iceman/carcpu/modules/input/Speedometer.java
@@ -19,9 +19,6 @@ public class Speedometer extends InputModule {
     private static final double CAR_DISTANCE_PER_ROTATION = ((CAR_TYRE_CIRCUNFERENCE_M) / (double) 1000);
     private static int wheelRotationCounter = 0;
     public static final byte SPEED_PULSE = (byte) 0b1011_0000;
-
-    private double speed = 0;
-    private int speedCounter = 0;
     private Timer speedDataTimer;
 
     public enum Type {
@@ -54,33 +51,10 @@ public class Speedometer extends InputModule {
                     firstSpeedFrequencyByte = commandValues[1];
                     secondSpeedFrequencyByte = commandValues[2];
 
-                    int frequency = (firstSpeedFrequencyByte & 0xFF) | ((secondSpeedFrequencyByte << 8) & 0xFF00);
-
-                    if (frequency > 0) {
-                        speed += (double) (frequency * 38) / (double) 210;
-                    }
-
-                    if (speedCounter == 16) {
-                        speed = speed / (double) speedCounter;
-
-                        if (speed > 220) {
-                            speed = 0;
-                        } else if (speed > 5) {
-                            getDashboard().setSpeed(speed);
-                        }
-
-                        wheelRotationCounter++;
-
-                        if (wheelRotationCounter == 55) {
-                            updateTripKilometers(0.1d);
-                            wheelRotationCounter = 0;
-                        }
-
-                        speed = 0;
-                        speedCounter = 0;
-
-                    } else {
-                        speedCounter++;
+                    int speed = (firstSpeedFrequencyByte & 0xFF) | ((secondSpeedFrequencyByte << 8) & 0xFF00);
+                    if (getDashboard().getRpm() > 0 && speed <= 220) {
+                        getDashboard().setSpeed(speed);
+                        updateTripKilometers(speed);
                     }
                 } catch (Exception e) {
                     createErrorMessage(carData, "Problem setting speed");
@@ -111,10 +85,15 @@ public class Speedometer extends InputModule {
         commands.add(SPEED_PULSE);
     }
 
-    private synchronized void updateTripKilometers(double value) {
-        getDashboard().setDistance(getDashboard().getDistance() + value);
+    private synchronized void updateTripKilometers(int speed) {
+        double distance = CAR_DISTANCE_PER_ROTATION;
+        if (speed >= 20) {
+            distance = distance * 2;
+        }
+
+        getDashboard().setDistance(getDashboard().getDistance() + distance);
         carSettings.setTripKilometers(getDashboard().getDistance());
-        getDashboard().setTotalDistance(getDashboard().getTotalDistance() + value);
+        getDashboard().setTotalDistance(getDashboard().getTotalDistance() + distance);
         carSettings.setConstantKilometers(getDashboard().getTotalDistance());
 
         if (speed > inputInterpreter.getCarTrip().getMaxSpeed()) {
